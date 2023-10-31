ff957ddd11e55d8e3f2c47881d1bfde9b92cd44c
Charles Morton <cgmorton@users.noreply.github.com>
2023-10-31 13:24:53 -0700

    Switching to pyproject.toml from setup.py

--- openet/ptjpl/__init__.py
+++ openet/ptjpl/__init__.py
@@ -1,7 +1,14 @@
+# try:
+#     from importlib import metadata
+# except ImportError:  # for Python<3.8
+#     import importlib_metadata as metadata
+
 from .collection import Collection
 from .image import Image
 from . import interpolate
 
-__version__ = "0.2.1"
-
 MODEL_NAME = 'PTJPL'
+
+# # __version__ = metadata.version(__package__ or __name__)
+# __version__ = metadata.version(__package__.replace('.', '-') or __name__.replace('.', '-'))
+# # __version__ = metadata.version('openet-ptjpl')
